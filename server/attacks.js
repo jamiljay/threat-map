14e3b4b14c21f140042d4175ad654aac7db21673
Jamil Shehadeh <me@jamiljay.xyz>
2018-06-26 00:39:23 -0400

    File for generating fake data.

---
+++ server/attacks.js
@@ -0,0 +1,41 @@
+const Chance = require('chance');
+
+const VIRUS_NAMES = [
+    "APT1",
+    "Spam",
+    "Botnet",
+    "StealCreds"
+];
+
+const FUNCTIONS = [
+    "web server",
+    "mail server",
+    "ftp server"
+];
+
+const chance = new Chance();
+
+
+function generateData () {
+    
+    const dataCount = chance.integer({ min: 0, max: 3 });
+
+    return {
+        "ip": chance.ip(),
+        "virus": VIRUS_NAMES.filter((virus, index)=>index <= dataCount),
+        "owner": chance.bool({ likelihood: 65 })? chance.name() : chance.company(),
+        "function": FUNCTIONS[ chance.integer({ min: 0, max: 2 }) ],
+        dataCount,
+        "coordinates": [
+            chance.longitude(),
+            chance.latitude()
+        ]
+    };
+}
+
+
+
+
+module.exports = {
+    generateData
+};

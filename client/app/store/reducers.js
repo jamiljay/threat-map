8a71f881ac2eb253a71e8ebe7983230434ba142c
Jamil Shehadeh <me@jamiljay.xyz>
2018-06-26 22:08:24 -0400

    Added data-counts for tracking virus counts.

--- client/app/store/reducers.js
+++ client/app/store/reducers.js
@@ -3,20 +3,47 @@ import Immutable from "immutable";
 import { CLEAR, LOAD, UPDATE } from "./actions";
 
 const init = Immutable.fromJS({
-	threats: []
+	threats: [],
+	dataCounts: {
+		trojans: 0,
+		bots: 0,
+		spam: 0,
+		total: 0
+	}
 });
+
+function threatCounts (threats, dataCounts) {
+	return Immutable.fromJS(threats.reduce((c,t)=>{
+		/* eslint-disable no-param-reassign */
+		if (t.virus.indexOf("APT1") > -1) c.trojans++;
+		if (t.virus.indexOf("StealCreds") > -1) c.trojans++;
+		if (t.virus.indexOf("Botnet") > -1) c.bots++;
+		if (t.virus.indexOf("Spam") > -1) c.spam++;
+
+		c.total += t.virus.length;
+		/* eslint-enable */
+
+		return c;
+	}, dataCounts.toJS()));
+}
 
 
 function appReducer(stateMap = init, action) {
+	let newState = null; 
+
 	switch (action.type) {
 		case CLEAR:
 			return stateMap.set("threats", Immutable.List());
 
 		case LOAD:
-			return stateMap.set("threats", Immutable.fromJS(action.threats));
+			newState = stateMap.set("dataCounts", threatCounts(action.threats, stateMap.get("dataCounts")));
+
+			return newState.set("threats", Immutable.fromJS(action.threats));
 
 		case UPDATE:
-			return stateMap.set("threats", stateMap.get("threats").concat(Immutable.fromJS(action.threats)));
+			newState = stateMap.set("dataCounts", threatCounts(action.threats, stateMap.get("dataCounts")));
+
+			return newState.set("threats", stateMap.get("threats").concat(Immutable.fromJS(action.threats)));
 
 		default:
 			return stateMap;

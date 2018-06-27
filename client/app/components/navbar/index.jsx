869b2bcf5a7c1645c72812be1b75f4d856a9b01d
Jamil Shehadeh <me@jamiljay.xyz>
2018-06-26 22:09:41 -0400

    Minimalized navbar. Added total threats count.

--- client/app/components/navbar/index.jsx
+++ client/app/components/navbar/index.jsx
@@ -1,90 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
-
-import * as Actions from '../../store/actions';
-
+  NavLink } from 'reactstrap';
 
 import './style.scss';
 
-class ThreatNav extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        isOpen: false
-      };
-    }
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
-    render () {
-
-        return (
-            <Navbar color="dark" light expand="sm">
-                <NavbarBrand href="/">Cyber Threat App</NavbarBrand>
-                <NavbarToggler onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav navbar>
-                        <NavItem>
-                            <NavLink onClick={()=>{}}>Map</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink onClick={()=>{}}>Grid</NavLink>
-                        </NavItem>
-                    </Nav>
-                    <Nav className="ml-auto" navbar>
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                            Viruses
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>
-                                    Trojans: 1
-                                </DropdownItem>
-                                <DropdownItem>
-                                    Bots: 2
-                                </DropdownItem>
-                                <DropdownItem>
-                                    Spam: 3
-                                </DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>
-                                    All: 6
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        );
-    }
-}
+const ThreatNav = (props) => (
+    <Navbar color="dark" light expand="sm">
+        <NavbarBrand href="/">Cyber Threat App</NavbarBrand>
+        <Nav navbar>
+            <NavItem>
+                <NavLink onClick={()=>{}}>Map</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink onClick={()=>{}}>Grid</NavLink>
+            </NavItem>
+        </Nav>
+        <Nav className="ml-auto" navbar>
+            <NavItem>
+                <NavLink onClick={()=>{}}>Threats {props.dataCounts.get("total").toLocaleString('en')}</NavLink>
+            </NavItem>
+        </Nav>
+    </Navbar>
+);
 
 function mapStateToProps(state) {
-    return { threats: state.app.get("threats").toJS() }
-}
-
-
-function mapDispatchToProps(dispatch){
-    return {
-        update: threats=>dispatch(Actions.update(threats)),
-        load: ()=>dispatch(Actions.load())
+    return { 
+        dataCounts: state.app.get("dataCounts")
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ThreatNav);
+export default connect(mapStateToProps)(ThreatNav);
